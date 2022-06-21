9a512a880f3ffaf27d7e6783e68a9d2774b1f9db
TobeyKwon <tobeykwon@gmail.com>
2022-06-22 02:09:06 +0900

    Post 앤티티 작성

---
+++ back-end/blog/src/main/java/com/example/blog/entity/Post.java
@@ -0,0 +1,33 @@
+package com.example.blog.entity;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Post extends BaseEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String writer;
+
+    private int view;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    @OrderBy("id asc")
+    private List<Reply> comments = new ArrayList<>();
+}
