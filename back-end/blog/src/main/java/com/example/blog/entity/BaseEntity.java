a8e704d4b988b54d7ad8ebe2f430ab7a78383a53
TobeyKwon <tobeykwon@gmail.com>
2022-06-22 02:10:54 +0900

    BaseEntity 오타 수정

--- back-end/blog/src/main/java/com/example/blog/entity/BaseEntity.java
+++ back-end/blog/src/main/java/com/example/blog/entity/BaseEntity.java
@@ -3,10 +3,7 @@ package com.example.blog.entity;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
@@ -22,7 +19,7 @@ public abstract class BaseEntity {
         this.createdDate = LocalDateTime.now();
     }
 
-    @PrePersist
+    @PreUpdate
     public void preUpdate() {
         this.updatedDate = LocalDateTime.now();
     }
