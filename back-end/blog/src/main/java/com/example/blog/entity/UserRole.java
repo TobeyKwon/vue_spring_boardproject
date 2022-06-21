fcfdd1d69f690ade7a2595f77d27078cbf0e55d8
TobeyKwon <tobeykwon@gmail.com>
2022-06-22 02:11:10 +0900

    UserRole 열거형 작성

---
+++ back-end/blog/src/main/java/com/example/blog/entity/UserRole.java
@@ -0,0 +1,15 @@
+package com.example.blog.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String value;
+
+    UserRole(String value) {
+        this.value = value;
+    }
+}
